4e59377ecff78410045f2c667122caddd5364daa
Strigon009 <42129076+Strigon009@users.noreply.github.com>
2020-08-18 13:40:42 +0800

    Add all files

---
+++ DM2231_Base_Framework/App/Source/Scene3D/ArmorPickup.h
@@ -0,0 +1,76 @@
+/**
+ CArmorPickup
+ By: Toh Da Jun
+ Date: Apr 2020
+ */
+#pragma once
+
+ // Include Entity3D
+#include <Primitives/Entity3D.h>
+
+// Include GLM
+#include <includes/glm.hpp>
+#include <includes/gtc/matrix_transform.hpp>
+#include <includes/gtc/type_ptr.hpp>
+
+// Include Camera
+#include "Camera.h"
+
+// Include Player3D
+#include "Player3D.h"
+
+// Include GroundMap
+#include "GroundMap.h"
+
+#include "..\Scene3D\Player3D.h"
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+class CArmorPickup : public CEntity3D
+{
+public:
+	// Default Constructor
+	CArmorPickup(void);
+
+	// Constructor with vectors
+	CArmorPickup(const glm::vec3 vec3Position,
+		const glm::vec3 vec3Front = glm::vec3(0.0f, 0.0f, 1.0f),
+		const float fYaw = 0.0f,
+		const float fPitch = 0.0f);
+
+	// Destructor
+	virtual ~CArmorPickup(void);
+
+	// Initialise this class instance
+	bool Init(void);
+
+	// Set model
+	virtual void SetModel(glm::mat4 model);
+	// Set view
+	virtual void SetView(glm::mat4 view);
+	// Set projection
+	virtual void SetProjection(glm::mat4 projection);
+
+	// Update this class instance
+	virtual void Update(const double dElapsedTime);
+
+	// Activate the CCollider for this class instance
+	virtual void ActivateCollider(Shader* cLineShader = NULL);
+
+	// PreRender
+	virtual void PreRender(void);
+	// Render
+	virtual void Render(void);
+	// PostRender
+	virtual void PostRender(void);
+
+	int iAddArmor;
+
+protected:
+	// The handle to the CGroundMap class instance
+	CGroundMap* cGroundMap;
+
+	// Add more things
+
+	// Handler to the Player3D class
+	CPlayer3D* cPlayer3D;
+};
